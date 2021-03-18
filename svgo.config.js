b1dba038f70955bc5f18d25eb63fffeec9e643a7
Todd Moy <todd.moy@gmail.com>
2021-03-18 11:24:39 -0600

    Update svgo settings and re-optimize

--- svgo.config.js
+++ svgo.config.js
@@ -1,5 +1,14 @@
 const { extendDefaultPlugins } = require('svgo');
 
+// Settings based on https://feb.svg-is-smashing.com/#/optimising?id=post-export-optimising.
+// I omitted anything that would reduce the fidelity of path data.
+
 module.exports = {
-  plugins: extendDefaultPlugins([{ name: 'removeDimensions', params: {} }]),
+  plugins: extendDefaultPlugins([
+    { name: 'cleanupIDs', active: false },
+    { name: 'mergePaths', active: false },
+    { name: 'removeXMLNS', params: {} },
+    { name: 'collapseGroups', active: false },
+    { name: 'removeDimensions', params: {} },
+  ]),
 };
